f683aa9c3e7327c295c3139a90a8ab2dfd345eae
Nagaraj-Edke <enagaraj9109@gmail.com>
2021-05-06 13:41:12 +0530

    change

--- FirstApp.js
+++ FirstApp.js
@@ -2,7 +2,7 @@ var http = require('http')
 
 http.createServer((req,res)=>{
 res.write("hello")
-    
+
     res.end("Hello world");
 }).listen(8081);
 //namesthe
@@ -10,4 +10,8 @@ res.write("hello")
 console.log("hello")
 
 console.log("Server is running at  http://localhost:8081/  **");
+console.log(true);
+res.writable;console.log(true);
+res.writable;console.log(true);
+res.writable; 
 
